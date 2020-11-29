71bcd9abf90c5845f10e3063019bf8a1e2328490
Stephanie Chui <s.chui@wustl.edu>
2020-11-28 16:33:15 -0800

    changes

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
+
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//       $(".zoom img").css({
+//           width:(100+scroll/5)+"%"
+//       });
+//   });
+
+AOS.init({
+    duration: 1200,
+  })
+
 window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 const sections = [...document.querySelectorAll("section")];
 
